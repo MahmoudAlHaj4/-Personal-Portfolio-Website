2d700225c995f5a8d5a480de07a894569d8dd426
MahmoudAlHaj4 <mahmoudalhaj509@gmail.com>
2024-05-09 07:49:59 +0300

    add hero section

--- personalwebsite/src/profile/profile.jsx
+++ personalwebsite/src/profile/profile.jsx
@@ -20,6 +20,42 @@ const Profile = ()=>{
                     
                 </div>
             </div>
+
+            <section>
+                <div className="home flex center">
+                    <div className="home-container flex column">
+                        <div className="hero-section">
+                            <h1>Hi It's <span>Mahmoud</span></h1>
+                            <h3 className="text-animation">I'm a <span></span></h3>
+                            <p>Enthusiastic Full Stack Developer with a knack for problem solving, I'm driven to create efficient digital experiences.
+                                Looking forward to join a team that can challenge me and introduce me to new experiences. 
+                                Let's connect and achieve something amazing together! </p>
+                        </div>
+                        <div className="social-icons flex">
+                            <a href=""><LinkedInIcon className="icon"/></a>
+                            <a href=""><GitHubIcon className="icon"/></a>
+                            <a href=""><InstagramIcon className="icon"/></a>
+                            <a href=""><XIcon className="icon"/></a>
+                        </div>
+
+                        <div className="btn-group flex">
+                            <button className="btn">Hire</button>
+                            <button className="btn">Contact</button>
+                        </div>
+                    </div>
+
+                <div className="home-img">
+                        <img src={image} alt="" />
+                </div>
+            </div>
+            </section>
+
+
+
+
+
+           
+
         </div>
     )
 }
