72319933ab50a6deda89d6dce7f6cf915cc428f4
MahmoudAlHaj4 <mahmoudalhaj509@gmail.com>
2024-05-09 07:48:07 +0300

    imort Icons

--- personalwebsite/src/profile/profile.jsx
+++ personalwebsite/src/profile/profile.jsx
@@ -1,4 +1,10 @@
-
+import "./profile.css"
+import Button from "./componenet/buttonComponenet"
+import image from '../images/sec.jpg'
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import XIcon from '@mui/icons-material/X';
+import InstagramIcon from '@mui/icons-material/Instagram';
 
 const Profile = ()=>{
     return (
